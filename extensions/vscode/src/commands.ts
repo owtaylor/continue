f7adcf21c3116ce69f0436759f2c24d29ce0eb65
Owen W. Taylor <otaylor@fishsoup.net>
2025-01-09 10:28:13 -0500

    WIP: Adapt the Paver GUI to its new role
    * Remove model selection - instead the model we show status for is
      based on what's in the Continue configuration.
    * Remove the step to write a new Continue configuration.
    * Lightly adapt text

--- extensions/vscode/src/commands.ts
+++ extensions/vscode/src/commands.ts
@@ -1012,7 +1012,7 @@ const getCommandsMap: (
       });
     },
     "granite.setup": () => {
-      SetupGranitePage.render(extensionContext);
+      SetupGranitePage.render(extensionContext, configHandler);
     }
   };
 };
