Granite-Code/continue-for-granite

Import Paver code as setupGranite
Import the Paver code for setting up Ollama and the Granite Models
into the extension without adaption other than to get it to build
and run within the Continue plugin. The granite.setup command will
open a Webview with the Continue GUI.

extensions/vscode/src/commands.ts
@@ -38,6 +38,8 @@ import { VsCodeIde } from "./VsCodeIde";
 
 import type { VsCodeWebviewProtocol } from "./webviewProtocol";
 
+import { SetupGranitePage } from "./setupGranite/panels/setupGranitePage";
+
 let fullScreenPanel: vscode.WebviewPanel | undefined;
 
 function getFullScreenTab() {
@@ -1007,6 +1009,9 @@ const getCommandsMap: (
           }
       });
     },
+    "granite.setup": () => {
+      SetupGranitePage.render(extensionContext);
+    }
   };
 };
 

owtaylor/continue

extensions/vscode/src/commands.ts
@@ -34,6 +34,8 @@ import { VsCodeIde } from "./VsCodeIde";
 
 import type { VsCodeWebviewProtocol } from "./webviewProtocol";
 
+import { SetupGranitePage } from "./granite/panels/setupGranitePage";
+
 let fullScreenPanel: vscode.WebviewPanel | undefined;
 
 function getFullScreenTab() {
@@ -1009,6 +1011,9 @@ const getCommandsMap: (
           }
       });
     },
+    "granite.setup": () => {
+      SetupGranitePage.render(extensionContext);
+    }
   };
 };
 