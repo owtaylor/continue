2536d3d3630561a32479eb3af7a9394b87e19e8e
Fred Bricon <fbricon@gmail.com>
2025-01-15 11:35:05 +0100

    Granite.Code: disable Continue's Onboarding UI
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- gui/src/components/OnboardingCard/hooks/useOnboardingCard.ts
+++ gui/src/components/OnboardingCard/hooks/useOnboardingCard.ts
@@ -7,6 +7,8 @@ import { getLocalStorage, setLocalStorage } from "../../../util/localStorage";
 import { useAppDispatch, useAppSelector } from "../../../redux/hooks";
 import { saveCurrentSession } from "../../../redux/thunks/session";
 
+const disableContinueOnboarding = true;
+
 export interface UseOnboardingCard {
   show: OnboardingCardState["show"];
   activeTab: OnboardingCardState["activeTab"];
@@ -37,6 +39,9 @@ export function useOnboardingCard(): UseOnboardingCard {
   }
 
   async function open(tab: TabTitle) {
+    if (disableContinueOnboarding) {
+      return;
+    }
     navigate("/");
 
     // Used to clear the chat panel before showing onboarding card
@@ -59,7 +64,7 @@ export function useOnboardingCard(): UseOnboardingCard {
   }
 
   return {
-    show,
+    show: !disableContinueOnboarding && show,
     setActiveTab,
     open,
     close,
